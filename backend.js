5bc4b2410774c960dcd6db0049332ddcbc59d1e2
laabroo <laabroo@laabroo-laode.(none)>
2011-12-16 02:05:00 +0700

    masih kurang banyak

--- backend.js
+++ backend.js
@@ -1,37 +1,38 @@
 // libcinta -- backend.js
 log.info('Hello from backend bootstrap.');
+var _ = require('underscore');
+var scaling = new (require('blaast/scaling').Scaling)();
 var http = require('blaast/simple-http');
+var QS = require('querystring');
+var url = require('url');
 var QS = require('querystring');
 
 app.message(function(client, action, data) {
     console.log('Action : ' + action);
     if (action === 'loadData') {
         var param = 'PustakaCinta';
-        var url = 'https://api.twitter.com/1/statuses/user_timeline.json?screen_name='+param;
+        var url = 'https://api.twitter.com/1/statuses/user_timeline.json?screen_name=' + param;
 
         http.get(url, {
             type: 'binary'
         }, {
             ok: function(data) {
-                var jsonObj = JSON.parse(data);
-                log.info(jsonObj);
-                console.log(jsonObj.text);
-                var array = [jsonObj];
-                console.log(array.length);
-                array.forEach(function(item){
+                console.log('Data JSON : '+data);
+                data = JSON.parse(data);
+                console.log(data);
+                var array = [data];
+                array.forEach(function(item) {
                     console.log(item.text);
                     var panjang = item.length;
-                    var i=0;
-                    for(i=0;i<panjang;i++){
-                        console.log(item.text[i]);
+                    console.log('Pangjang : '+panjang);
+                    var i = 0;
+                    for(i = 0; i < panjang; i++) {
                         client.msg('loadData', {
                             text :
-                            { content : item.text[i]}
-                            });
+                            { content : item[i].text}
+                        });
                     }
-                    
-                    });
-                
+                });
             },
             error: function(err) {
                 console.log(err);
--- controller/main.js
+++ controller/main.js
@@ -1,64 +1,124 @@
 var _ = require('common/util');
+var app = this;
+var ui = require('ui');
+var TextView = ui.TextView;
+var VLayout = require('ui').VLayout;
+var Control = require('ui').Control;
 
 var TAG = 'main.js';
 _.extend(exports, {
-	':load': function() {
-		console.log('View was loaded');
-		var view = this;
+    ':load': function() {
+        console.log('View was loaded');
+        var view = this;
+        app.on('connected', function() {
+            app.msg('loadData', { data : ''});
+            app.on('message', function(action, data) {
+                if (action === 'loadData') {
+                    console.log(data.text.content);
+                    clearInterval(view.intervalId);
+                    delete view.intervalId;
+                    var i = 1;
+                    var temp;
+                    var dataArray = [data.text.content];
+                    dataArray.forEach(function(item) {
+                        console.log('Item : ' + item);
+                        if (i % 2 === 0) {
+                            temp = new TextView({
+                                label: item,
+                                style: {
+                                    color: 'black',
+                                    width: 'fill-parent',
+                                    'font-size':'small',
+                                    'background-color': 'transparent'
+                                }
+                            });
+                            temp.on('blur', function() {
+                                this.style({
+                                    'color': 'black',
+                                    'font-weight': 'normal',
+                                    'background-color': 'transparent'
+                                });
+                            });
+                        } else {
+                            temp = new TextView({
+                                label: item,
+                                style: {
+                                    color: 'black',
+                                    'font-size':'small',
+                                    width: 'fill-parent',
+                                    'background-color': '#BDB76B'
+                                }
+                            });
+                            temp.on('blur', function() {
+                                this.style({
+                                    'color': 'black',
+                                    'background-color': '#BDB76B',
+                                    'font-weight': 'normal'
+                                });
+                            });
+                        }
+                        temp.on('activate', function() {
+                            app.setContent('main', {
+                                title: data.text.content
+                            });
+                        });
+                        temp.on('focus', function() {
+                            this.style({
+                                'color': 'black',
+                                'background-color': '#3682b0',
+                                'font-weight': 'bold'
+                            });
+                        });
+                        view.add(item, temp);
+                        i++;
+                    });
+                    view.focusItem(1);
+                }
 
-		app.view('main').on('load', function() {
-			console.log(TAG + ' : ---------------- *******************  -------------------');
-			app.on('message', function(action, data) {
-				if (action === 'loadData') {
-					console.log('Action : ' + action);
-					console.log(data.text.content);
-				}
-			});
-		});
-	},
-	':state': function() {
-		app.on('message', function(action, data) {
-			if (action === 'loadData') {
-				console.log('Action : ' + action);
-			}
-		});
-	},
+            });
+        });
 
-	':keypress': function(key) {
-		if (this.index === undefined) {
-			if (this.size() > 0) {
-				this.focusItem(1);
-			}
-		} else if (key === 'up' || key === 'down') {
-			var next = this.index + (key === 'up' ? -1 : 1);
+    },
 
-			if (next < 1) {
-				next = 1;
-			} else if (next > (this.size() - 1)) {
-				next = this.size() - 1;
-			}
+    ':keypress'
+        :
+        function(key) {
+            if (this.index === undefined) {
+                if (this.size() > 0) {
+                    this.focusItem(1);
+                }
+            } else if (key === 'up' || key === 'down') {
+                var next = this.index + (key === 'up' ? -1 : 1);
 
-			if (this.index === next) {
-				return;
-			}
+                if (next < 1) {
+                    next = 1;
+                } else if (next > (this.size() - 1)) {
+                    next = this.size() - 1;
+                }
 
-			this.focusItem(next);
-		} else if (key === 'fire') {
-			this.get(this.index).emit('activate');
-		}
-	},
+                if (this.index === next) {
+                    return;
+                }
 
-	focusItem: function(index) {
-		if (this.index !== undefined) {
-			this.get(this.index).emit('blur');
-		}
-		this.index = index;
-		this.get(index).emit('focus');
-		if (index === 1) {
-			this.scrollTop(0);
-		}
-		console.log(index);
-		this.scrollTo(index);
-	}
+                this.focusItem(next);
+            } else if (key === 'fire') {
+                this.get(this.index).emit('activate');
+            }
+        }
+    ,
 
-});
+    focusItem: function(index) {
+        if (this.index !== undefined) {
+            this.get(this.index).emit('blur');
+        }
+        this.index = index;
+        this.get(index).emit('focus');
+        if (index === 1) {
+            this.scrollTop(0);
+        }
+        console.log(index);
+        this.scrollTo(index);
+    }
+
+})
+    ;
